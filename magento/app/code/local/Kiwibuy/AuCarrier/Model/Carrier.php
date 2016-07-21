1b6503f08b9f1408249428eda5940cbddbb3e9e6
juno <jun.chenying@hotmail.com>
2016-07-21 17:17:56 +1000

    special product shipping rule

--- magento/app/code/local/Kiwibuy/AuCarrier/Model/Carrier.php
+++ magento/app/code/local/Kiwibuy/AuCarrier/Model/Carrier.php
@@ -5,10 +5,15 @@ class Kiwibuy_AuCarrier_Model_Carrier
 {
     protected $_code = 'kiwibuy_aucarrier';
 
+    public $_special_cata_price = 9;
+    public $_special_category_id = 20;
+
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
-        $result = Mage::getModel('shipping/rate_result');
 
+        $this->_special_cata_price = $this->getConfigData('special_cata_price');
+        $this->_special_category_id = $this->getConfigData('special_category_id');
+        $result = Mage::getModel('shipping/rate_result');
         /* @var $result Mage_Shipping_Model_Rate_Result */
         $result->append($this->_getStandardShippingRate($request));
         return $result;
@@ -16,6 +21,8 @@ class Kiwibuy_AuCarrier_Model_Carrier
 
     protected function _getStandardShippingRate($request)
     {
+        $special_cata_price = $this->_special_cata_price;
+        $special_category_id = $this->_special_category_id;
         $price = $this->getConfigData('price');
         $itemQtythThreshold = $this->getConfigData('item_qty_threshold');
         $totalQtythThreshold = $this->getConfigData('total_qty_threshold');
@@ -23,6 +30,8 @@ class Kiwibuy_AuCarrier_Model_Carrier
 
         $totalQty = 0;
         $eligibleItemQty = True;
+        $special_cata_total_price = 0;
+        $special_cata_total_qty = 0;
 
         foreach ($request->getAllItems() as $item) {
             if ($item->getParentItemId()) {
@@ -31,12 +40,23 @@ class Kiwibuy_AuCarrier_Model_Carrier
             if ($item->getQty() > $itemQtythThreshold){
                 $eligibleItemQty = false;
             }
-            $totalQty = $totalQty + $item->getQty();
+            $product = Mage::getModel('catalog/product')->load($item->getProductId()); 
+            $categoryIds = $product->getCategoryIds();
+            if (in_array($special_category_id, $categoryIds)){
+                $special_cata_total_qty = $special_cata_total_qty + $item->getQty();
+            } else {
+                $totalQty = $totalQty + $item->getQty(); //except special catagory
+            }
         }    
+        //special catagory
+        $special_cata_total_price = $special_cata_total_qty * $special_cata_price;
+
+        //except special catagory
         if ($totalQty > $totalQtythThreshold) {
             $price = $price + ($totalQty - $totalQtythThreshold) * $addPricePerItem;
         }
         if ($eligibleItemQty) {
+            $price = $price + $special_cata_total_price;
             $rate = Mage::getModel('shipping/rate_result_method');
             /* @var $rate Mage_Shipping_Model_Rate_Result_Method */
 
@@ -49,7 +69,6 @@ class Kiwibuy_AuCarrier_Model_Carrier
 
             $rate->setMethod('standand');
             $rate->setMethodTitle('Standard');
-
             $rate->setPrice($price);
             $rate->setCost(0);
         }
--- magento/app/code/local/Kiwibuy/SaCarrier/Model/Carrier.php
+++ magento/app/code/local/Kiwibuy/SaCarrier/Model/Carrier.php
@@ -5,8 +5,14 @@ class Kiwibuy_SaCarrier_Model_Carrier
 {
     protected $_code = 'kiwibuy_sacarrier';
 
+    public $_special_cata_price = 9;
+    public $_special_category_id = 20;
+
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
+        $this->_special_cata_price = $this->getConfigData('special_cata_price');
+        $this->_special_category_id = $this->getConfigData('special_category_id');
+
         $result = Mage::getModel('shipping/rate_result');
         /* @var $result Mage_Shipping_Model_Rate_Result */
         $result->append($this->_getDhlShippingRate($request));
@@ -15,6 +21,9 @@ class Kiwibuy_SaCarrier_Model_Carrier
 
     protected function _getDhlShippingRate($request)
     {
+        $special_cata_price = $this->_special_cata_price;
+        $special_category_id = $this->_special_category_id;
+        
         $range_one_price = $this->getConfigData('range_one_price');
         $range_one_itemQtythThreshold = $this->getConfigData('range_one_item_qty_threshold');
         $range_one_totalQtythThreshold = $this->getConfigData('range_one_total_qty_threshold');
@@ -25,6 +34,8 @@ class Kiwibuy_SaCarrier_Model_Carrier
 
         $totalQty = 0;
         $eligibleItemQty = True;
+        $special_cata_total_price = 0;
+        $special_cata_total_qty = 0;
 
         foreach ($request->getAllItems() as $item) {
             if ($item->getParentItemId()) {
@@ -33,8 +44,18 @@ class Kiwibuy_SaCarrier_Model_Carrier
             if ($item->getQty() > $range_one_itemQtythThreshold || $item->getQty() > $range_two_itemQtythThreshold){
                 $eligibleItemQty = false;
             }
-            $totalQty = $totalQty + $item->getQty();
+            $product = Mage::getModel('catalog/product')->load($item->getProductId()); 
+            $categoryIds = $product->getCategoryIds();
+            if (in_array($special_category_id, $categoryIds)){
+                $special_cata_total_qty = $special_cata_total_qty + $item->getQty();
+            } else {
+                $totalQty = $totalQty + $item->getQty(); //except special catagory
+            }
         }    
+        //special catagory
+        $special_cata_total_price = $special_cata_total_qty * $special_cata_price;
+
+        //except special catagory
         if ($totalQty <= $range_one_totalQtythThreshold) {
             $price = $range_one_price;
         } else if ($totalQty > $range_one_totalQtythThreshold && $totalQty <= $range_two_totalQtythThreshold) {
@@ -44,6 +65,7 @@ class Kiwibuy_SaCarrier_Model_Carrier
             $price = $range_two_price + ($totalQty - $range_two_totalQtythThreshold) * $addPricePerItem;
         }
         if ($eligibleItemQty) {
+            $price = $price + $special_cata_total_price;
             $rate = Mage::getModel('shipping/rate_result_method');
             /* @var $rate Mage_Shipping_Model_Rate_Result_Method */
 
