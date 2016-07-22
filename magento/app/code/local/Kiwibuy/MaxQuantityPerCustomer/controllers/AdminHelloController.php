867e9f2ad16b9e7d9e307908dbed1c6f1ee83f4d
juno <jun.chenying@hotmail.com>
2016-07-22 16:09:03 +1000

    admin config

---
+++ magento/app/code/local/Kiwibuy/MaxQuantityPerCustomer/controllers/AdminHelloController.php
@@ -0,0 +1,9 @@
+<?php
+class Kiwibuy_MaxQuantityPerCustomer_AdminhelloController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+} 
