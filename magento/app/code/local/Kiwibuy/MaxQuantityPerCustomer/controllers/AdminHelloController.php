674994d56a3959fc70da155f8bd1195aa2df2281
juno <jun.chenying@hotmail.com>
2016-07-25 17:30:17 +1000

    aaaaaaa

--- magento/app/code/local/Kiwibuy/MaxQuantityPerCustomer/controllers/AdminHelloController.php
+++ magento/app/code/local/Kiwibuy/MaxQuantityPerCustomer/controllers/AdminHelloController.php
@@ -3,6 +3,10 @@ class Kiwibuy_MaxQuantityPerCustomer_AdminhelloController extends Mage_Adminhtml
 {
     public function indexAction()
     {
+    	echo "string";
+    	$helper = Mage::helper('kiwibuy_maxquantitypercustomer');
+    	$configMsg=$helper->isModuleEnabled();
+    	var_dump($configMsg);
         $this->loadLayout();
         $this->renderLayout();
     }
